90e4ee7f104eb258436c2744b51f6398efa3925d
Juan Almanza <juancaoo300@hotmail.com>
2024-01-18 08:28:34 -0500

    Contact Mobile - Juan

--- src/components/form-contact.tsx
+++ src/components/form-contact.tsx
@@ -18,7 +18,7 @@ export default function FormContact({ backgroundImage, titleForm }: IProps) {
             ></Image>
             <div className="position-relative">
               <div className="container">
-                <h1 className="m-4 fs-3 position relative">
+                <h1 className="p-4 fs-3 position relative">
                   Contact us for{" "}
                   <strong>
                     Fast, Easy and <br />
@@ -62,87 +62,87 @@ export default function FormContact({ backgroundImage, titleForm }: IProps) {
             </div>
           </div>
         </div>
-        
-          <div className="md-w-50 w-100  bg-dark-blue text-white">
-            <form
-              action="https://flow.zoho.com/785473680/flow/webhook/incoming?zapikey=1001.1efe7f16cde72a5dc615d742476cc36e.fe77873c5c71e0bc95b7b8bb11dddbb8&isdebug=false"
-              method="POST"
-              id="form"
-              className="m-3rem"
-              target="#"
-            >
-              <h1
-                className=" fs-2 text-left fst-italic"
-                style={{ marginBottom: "30px" }}
-              >
-                {titleForm}
-                <div className="line "></div>
-              </h1>
-              <div className="row mb-3 flex-md-row flex-column">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control px-3 bg-grey-transparent text-white"
-                    id="name"
-                    name="name"
-                    placeholder="Full Name"
-                  ></input>
-                </div>
-                <div className="col">
-                  <input
-                    type="email"
-                    className="form-control px-3 bg-grey-transparent text-white"
-                    id="email"
-                    name="email"
-                    placeholder="Your E-Mail"
-                  ></input>
-                </div>
-              </div>
-              <div className="row mb-3 flex-md-row flex-column">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control px-3 bg-grey-transparent text-white"
-                    id="company"
-                    name="company"
-                    placeholder="Company Name"
-                  ></input>
-                </div>
-                <div className="col">
-                  <input
-                    type="number"
-                    className="form-control px-3 bg-grey-transparent text-white"
-                    id="phone"
-                    name="phone"
-                    placeholder="Phone"
-                  ></input>
-                </div>
-              </div>
-              <div className="mb-3">
-                <textarea
-                  className="form-control bg-grey-transparent text-white"
-                  id="message"
-                  name="message"
-                  rows={3}
-                  placeholder="Description"
-                ></textarea>
-              </div>
-              <div className="mb-3 form-check d-flex justify-content-between">
-                <div>
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="language"
-                    name="language"
-                  ></input>
-                  <label className="form-check-label">Spanish</label>
-                </div>
 
-                <button type="submit" className="btn fw-600">
-                  SEND
-                </button>
+        <div className="md-w-50 w-100  bg-dark-blue text-white">
+          <form
+            action="https://flow.zoho.com/785473680/flow/webhook/incoming?zapikey=1001.1efe7f16cde72a5dc615d742476cc36e.fe77873c5c71e0bc95b7b8bb11dddbb8&isdebug=false"
+            method="POST"
+            id="form"
+            className="m-3rem"
+            target="#"
+          >
+            <h1
+              className=" fs-2 text-left fst-italic"
+              style={{ marginBottom: "30px" }}
+            >
+              {titleForm}
+              <div className="line "></div>
+            </h1>
+            <div className="row mb-3 flex-md-row flex-column">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control px-3 bg-grey-transparent text-white"
+                  id="name"
+                  name="name"
+                  placeholder="Full Name"
+                ></input>
               </div>
-            </form>
+              <div className="col">
+                <input
+                  type="email"
+                  className="form-control px-3 bg-grey-transparent text-white"
+                  id="email"
+                  name="email"
+                  placeholder="Your E-Mail"
+                ></input>
+              </div>
+            </div>
+            <div className="row mb-3 flex-md-row flex-column">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control px-3 bg-grey-transparent text-white"
+                  id="company"
+                  name="company"
+                  placeholder="Company Name"
+                ></input>
+              </div>
+              <div className="col">
+                <input
+                  type="number"
+                  className="form-control px-3 bg-grey-transparent text-white"
+                  id="phone"
+                  name="phone"
+                  placeholder="Phone"
+                ></input>
+              </div>
+            </div>
+            <div className="mb-3">
+              <textarea
+                className="form-control bg-grey-transparent text-white"
+                id="message"
+                name="message"
+                rows={3}
+                placeholder="Description"
+              ></textarea>
+            </div>
+            <div className="mb-3 form-check d-flex justify-content-between">
+              <div>
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="language"
+                  name="language"
+                ></input>
+                <label className="form-check-label">Spanish</label>
+              </div>
+
+              <button type="submit" className="btn fw-600">
+                SEND
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
