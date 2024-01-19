2db9f59ebafeb2a8f89221022903baa7a83e1a42
Juan Almanza <juancaoo300@hotmail.com>
2024-01-18 21:56:11 -0500

    Footer Mobile - Juan

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -50,26 +50,29 @@ export default function Footer() {
               </Nav.Link>
             </Nav>
 
-            <button
-              id="bottom-footer"
-              className="btn bg-green text-dark-blue fw-600 mt-3"
-            >
-              CURIOUS TO LEARN MORE?
-            </button>
+            <div className="d-flex">
+              <div className="py-5 mx-3">
+                <button
+                  id="bottom-footer"
+                  className="btn bg-green text-dark-blue fw-600 mt-3"
+                >
+                  CURIOUS TO LEARN MORE?
+                </button>
+              </div>
+              <div className="text-center py-5">
+                <Image
+                  src="images/sff-logo-footer.svg"
+                  width="174"
+                  height="54"
+                  className="d-inline-block align-top"
+                  alt="SFF Logo"
+                />
+              </div>
+            </div>
           </Container>
 
-          <div className="text-center py-5">
-            <Image
-              src="images/sff-logo-footer.svg"
-              width="174"
-              height="54"
-              className="d-inline-block align-top"
-              alt="SFF Logo"
-            />
-          </div>
-
-          <div className="  font-montserrat d-flex position-relative justify-content-between align-items-center text-left py-3 px-5 bg-green">
-            <p style={{ margin: 0 }}>
+          <div className="font-montserrat d-flex position-relative justify-content-between align-items-center text-left py-3 px-5 bg-green">
+            <p className="w-100 mx-auto" style={{ margin: 0, fontSize: 15 }}>
               &copy;2023 Smart Funding Team All Rights Reserved
             </p>
             <div
