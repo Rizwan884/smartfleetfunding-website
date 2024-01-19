5cbe5dcfb1e5fbe3bc1eeb98b94c9e74846d3633
Eduardo Almanza <ealmanza@russ.com.co>
2024-01-19 17:03:50 -0500

    fix mx px row sections

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -30,7 +30,7 @@ export default function Banners({
           alt="banner"
         />
         {isMobile ? (
-          <div className="banner-container mt-n2 ms-n2">
+          <div className="banner-container mt-n2 ms-n2 w-75">
             <div className="mt-4 line"></div>
             {title && (
               <div>
