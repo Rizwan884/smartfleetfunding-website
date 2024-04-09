e392f2c73899ba3fa9841df6f872084ed0e32f21
Eduardo Almanza <ealmanza@russ.com.co>
2024-04-09 16:55:50 -0500

    validations

--- src/app/api/submitForm/route.ts
+++ src/app/api/submitForm/route.ts
@@ -3,12 +3,30 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
   const data = await new Response(req.body).text()
   const jsonData = JSON.parse(data)
+  const body = JSON.stringify({ ...jsonData })
+  const host = req.headers.get('host')
+  if (body === '{}')
+    return NextResponse.json({
+      success: false,
+      message: 'Invalid request body',
+      code: 400
+    })
+  if (
+    host !== 'smartfleetfunding.com' &&
+    host !== 'www.smartfleetfunding.com' &&
+    host !== 'localhost:3001'
+  )
+    return NextResponse.json({
+      success: false,
+      message: 'unauthorized',
+      code: 401
+    })
 
   const response = await fetch(
     'https://flow.zoho.com/785473680/flow/webhook/incoming?zapikey=1001.1efe7f16cde72a5dc615d742476cc36e.fe77873c5c71e0bc95b7b8bb11dddbb8&isdebug=false',
     {
       method: 'POST',
-      body: JSON.stringify({ ...jsonData }),
+      body: body,
       headers: {
         'Content-Type': 'application/json'
       }
