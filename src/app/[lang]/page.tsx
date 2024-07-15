e9fecce181cb5a8e66e7aa9e236490c239d2e59d
Juan Almanza <juancaoo300@hotmail.com>
2024-07-15 16:54:05 -0500

    Trustpilot

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import SectionVideo from '@/components/section-video'
+import Trustpilot from '@/components/trustpilot'
 import CardsSections from '../../components/cards_sections'
 import Footer from '../../components/footer'
 import Navbar from '../../components/navbar'
@@ -12,6 +13,7 @@ export default function Home() {
       <Slider />
       <CardsSections />
       <SectionVideo />
+      <Trustpilot />
       <Footer />
     </>
   )
